4df3b6dd4eb46771df9d7838366838733a8d35dd
Olamilekan Bakare <bakeolam@gmail.com>
2026-01-05 23:11:45 +0100

    Add files via upload

---
+++ advanced_Calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the First Number "))
+OP =    (input("Enter Operator "))
+num2 = float(input("Enter the Second Number "))
+
+if OP == "+":
+    print(num1 + num2)
+
+elif OP == "-":
+    print(num1 - num2)
+
+elif OP == "*":
+    print(num1 * num2)
+
+elif OP == "/":
+    print(num1 / num2)
+
+else:
+    print("Invalid....")
